4661a77c0b2f6fb085ad79d1ff497e4ce9b7a554
Neeraj Kashyap <nkashy1@gmail.com>
2022-04-15 09:12:00 -0700

    max recall then max precision to match facets to contracts
    Also list misses - methods on contracts which are not loaded on the
    diamond

--- inspector_facet/cli.py
+++ inspector_facet/cli.py
@@ -1,6 +1,6 @@
 import argparse
 import json
-import os
+import sys
 
 from brownie import network
 
@@ -8,10 +8,19 @@ from .abi import project_abis
 from .inspector import inspect_diamond
 from .DiamondLoupeFacet import add_default_arguments
 
+
 def main():
     parser = argparse.ArgumentParser(description="Inspector Facet")
     add_default_arguments(parser, transact=False)
-    parser.add_argument("-p", "--project", required=True, help="Path to brownie project")
+    parser.add_argument(
+        "-p", "--project", required=True, help="Path to brownie project"
+    )
+    parser.add_argument(
+        "--format",
+        choices=["json", "human"],
+        default="json",
+        help="Format in which to print output",
+    )
 
     args = parser.parse_args()
 
@@ -20,7 +29,14 @@ def main():
     network.connect(args.network)
 
     result = inspect_diamond(args.address, abis)
-    print(json.dumps(result))
+
+    if args.format == "json":
+        json.dump(result, sys.stdout)
+    elif args.format == "human":
+        raise NotImplemented
+    else:
+        raise ValueError(f"Unknown format: {args.format}")
+
 
 if __name__ == "__main__":
     main()
--- inspector_facet/inspector.py
+++ inspector_facet/inspector.py
@@ -36,27 +36,69 @@ def inspect_diamond(address: str, abis: Dict[str, Any]) -> Dict[str, Any]:
     for address, selectors in mounted_facets:
         facets[address] = [str(selector) for selector in selectors]
 
-    facet_matches: Dict[str, Dict[str, int]] = {}
-    max_matches: Dict[str, int] = {}
+    facet_recalls: Dict[str, Dict[str, float]] = {}
+    facet_precisions: Dict[str, Dict[str, int]] = {}
+    max_recall: Dict[str, float] = {}
     for address, selectors in facets.items():
-        facet_matches[address] = {}
+        if len(selectors) == 0:
+            continue
+        # Recall
+        facet_recalls[address] = {}
         for selector in selectors:
             selector_contracts = selector_index.get(selector)
             if selector_contracts is not None:
                 for contract_name in selector_contracts:
-                    if facet_matches[address].get(contract_name) is None:
-                        facet_matches[address][contract_name] = 0
-                    facet_matches[address][contract_name] += 1
-        max_matches[address] = max(value for _, value in facet_matches[address].items())
+                    if facet_recalls[address].get(contract_name) is None:
+                        facet_recalls[address][contract_name] = 0.0
+                    facet_recalls[address][contract_name] += 1.0
+        for contract_name in facet_recalls[address]:
+            facet_recalls[address][contract_name] /= len(selectors)
+
+        max_recall[address] = max(value for _, value in facet_recalls[address].items())
+
+        # Precision
+        facet_precisions[address] = {}
+        for contract_name, selectors in contract_selectors.items():
+            if len(selectors) == 0:
+                facet_precisions[address][contract_name] = 0.0
+                continue
+            facet_precisions[address][contract_name] = 0.0
+            for selector in selectors:
+                if selector in facets[address]:
+                    facet_precisions[address][contract_name] += 1.0
+            facet_precisions[address][contract_name] /= len(selectors)
 
     result: Dict[str, Dict[str, Any]] = {}
     for address, selectors in facets.items():
         address_result: Dict[str, Any] = {}
-        address_result["matches"] = [
-            match
-            for match, score in facet_matches[address].items()
-            if score == max_matches[address]
+        max_recall_contracts = [
+            contract_name
+            for contract_name in contract_selectors
+            if facet_recalls[address].get(contract_name, 0.0) == max_recall[address]
         ]
+        max_precision_for_max_recall_contracts = max(
+            precision
+            for contract_name, precision in facet_precisions[address].items()
+            if contract_name in max_recall_contracts
+        )
+        address_result["matches"] = [
+            contract_name
+            for contract_name in max_recall_contracts
+            if facet_precisions[address][contract_name]
+            == max_precision_for_max_recall_contracts
+        ]
+
+        address_result["misses"] = [
+            {
+                "contract": contract_name,
+                "selector": selector,
+                "function": function_name,
+            }
+            for contract_name in address_result["matches"]
+            for selector, function_name in contract_selectors[contract_name].items()
+            if selector not in facets[address]
+        ]
+
         address_result["selectors"] = {
             selector: [
                 {
@@ -68,6 +110,7 @@ def inspect_diamond(address: str, abis: Dict[str, Any]) -> Dict[str, Any]:
             ]
             for selector in selectors
         }
+
         result[address] = address_result
 
     return result
