7a268421e3c98143cc670e0324731f5cfa234335
Neeraj Kashyap <nkashy1@gmail.com>
2022-04-22 16:39:42 -0700

    Implemented --timeline mode

--- inspector_facet/cli.py
+++ inspector_facet/cli.py
@@ -1,22 +1,94 @@
 import argparse
 import json
 import sys
-from typing import Any, Dict
+from typing import Any, Dict, List, Optional, Tuple
 
 from .abi import project_abis
 from .facets import (
     events_from_moonworm_crawldata,
     facets_from_loupe,
     facets_from_events,
+    CUT_ACTION_ADD,
+    CUT_ACTION_REMOVE,
+    CUT_ACTION_REPLACE,
 )
 from .inspector import inspect_diamond
 from .version import VERSION
 
 
+def print_timeline_event_for_human(
+    result: Dict[str, Any],
+    maybe_previous_result: Optional[Dict[str, Any]],
+    event: Dict[str, Any],
+) -> None:
+    print(
+        f"# DiamondCut at block number {event['blockNumber']} (transaction hash: {event['transactionHash']})"
+    )
+
+    cuts = event["args"]["_diamondCut"]
+    initializer_address = event["args"]["_init"]
+    calldata = event["args"]["_calldata"]
+    print(
+        f"Cuts were made with initializer: {initializer_address} (calldata: {calldata})"
+    )
+
+    print("## Modifications")
+    for cut in cuts:
+        facet_address = cut[0]
+        action = cut[1]
+        selectors = cut[2]
+
+        if facet_address not in result:
+            print(f"### Facet at address: {facet_address}")
+            print("This address did not match any of the given contracts.")
+        else:
+            print(
+                f"This address could be one of the following contracts: {' or '.join(result[facet_address]['matches'])}."
+            )
+
+        action_text = "added"
+        if action == CUT_ACTION_REMOVE:
+            action_text = "removed"
+        elif action == CUT_ACTION_REPLACE:
+            action_text = "replaced"
+
+        print(f"The following selectors were {action_text} on the Diamond:")
+        all_selectors: List[Tuple[str, str, str]] = [
+            (item["contract"], item["function"], item["selector"])
+            for known_facet_address in result
+            for item in result[known_facet_address]["selectors"]
+        ]
+        if maybe_previous_result is not None:
+            all_selectors += [
+                (item["contract"], item["function"], item["selector"])
+                for known_facet_address in maybe_previous_result
+                for item in maybe_previous_result[known_facet_address]["selectors"]
+            ]
+        all_selectors = list(set(all_selectors))
+
+        selector_matches: Dict[str, List[Tuple[str, str]]] = {}
+        for selector in selectors:
+            selector_matches[selector] = []
+            for contract, function, known_selector in all_selectors:
+                if selector == known_selector:
+                    selector_matches[selector].append((contract, function))
+
+        for selector in selectors:
+            if not selector_matches[selector]:
+                print(f"\t{selector}")
+            else:
+                snippets = [
+                    f"{function} from {contract}"
+                    for contract, function in selector_matches[selector]
+                ]
+                print(f"\t{selector} - {' or '.join(snippets)}")
+    print("## Cumulative functionality")
+    print_result_for_human(result)
+
+
 def print_result_for_human(result: Dict[str, Any]) -> None:
     for address, address_result in result.items():
-        print("- - -")
-        print(f"Facet at address: {address}")
+        print(f"### Facet at address: {address}")
         print(f"Possible contracts: {', '.join(address_result['matches'])}")
         for contract_name in address_result["matches"]:
             misses = [
@@ -100,6 +172,29 @@ def main():
             json.dump(result, sys.stdout)
         elif args.format == "human":
             print_result_for_human(result)
+        else:
+            raise ValueError(f"Unknown format: {args.format}")
+    else:
+        if args.crawldata is None:
+            raise ValueError("--timeline mode can only be used with --crawldata")
+
+        diamond_cut_events = events_from_moonworm_crawldata(args.crawldata)
+        results_with_diffs: List[Tuple[Dict[str, Any], Dict[str, Any]]] = []
+        for i, event in enumerate(diamond_cut_events):
+            intermediate_result = inspect_diamond(
+                facets_from_events(diamond_cut_events[: i + 1]), abis
+            )
+            results_with_diffs.append((intermediate_result, event))
+
+        if args.format == "json":
+            json.dump(results_with_diffs, sys.stdout)
+        elif args.format == "human":
+            for i, item in enumerate(results_with_diffs):
+                result, event = item
+                maybe_previous_result: Optional[Dict[str, Any]] = None
+                if i > 0:
+                    maybe_previous_result = results_with_diffs[i - 1][0]
+                print_timeline_event_for_human(result, maybe_previous_result, event)
         else:
             raise ValueError(f"Unknown format: {args.format}")
 
